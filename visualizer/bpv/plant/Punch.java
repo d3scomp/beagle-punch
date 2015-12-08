2e48b91f5dc9ab8966936b22e668c1c059082694
Tomas Bures <tbures@gmail.com>
2015-12-08 19:54:37 +0100

    Initial commit of the visualizer

---
+++ visualizer/bpv/plant/Punch.java
@@ -0,0 +1,22 @@
+package bpv.plant;
+
+public class Punch
+{
+	private double x, y;
+
+	public Punch(double x, double y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX()
+	{
+		return this.x;
+	}
+
+	public double getY()
+	{
+		return this.y;
+	}
+}
